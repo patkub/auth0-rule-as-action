1c4b1e1bdd38b9ac1a9b99d9613609f4074acec6
Patrick Kubiak <epicpatka@gmail.com>
2025-09-21 00:14:56 -0400

    exampleRule unit test samlConfiguration

--- test/integration/exampleRule.test.js
+++ test/integration/exampleRule.test.js
@@ -33,12 +33,39 @@ describe("example rule", function () {
         configuration.TEST_SECRET;
       context.accessToken["https://example.com/testAccessToken"] =
         "testAccessTokenValue";
+
       // SAML
       context.samlConfiguration.mappings = {
         "https://example.com/SAML/Attributes/Role": "role",
         "https://example.com/SAML/Attributes/RoleSessionName": "session",
       };
+      context.samlConfiguration.audience =
+        "https://example.com/SAML/Attributes/Audience";
+      context.samlConfiguration.issuer =
+        "https://example.com/SAML/Attributes/Issuer";
+      context.samlConfiguration.encryptionPublicKey = `-----BEGIN PUBLIC KEY-----FAKE-----END PUBLIC KEY-----`;
+      context.samlConfiguration.recipient =
+        "https://example.com/SAML/Attributes/Recipient";
+      context.samlConfiguration.createUpnClaim = true;
+      context.samlConfiguration.passthroughClaimsWithNoMapping = true;
+      context.samlConfiguration.mapUnknownClaimsAsIs = true;
+      context.samlConfiguration.mapIdentities = true;
+      context.samlConfiguration.destination =
+        "https://example.com/SAML/Attributes/Destination";
+      context.samlConfiguration.relayState =
+        "https://example.com/SAML/Attributes/relayState";
       context.samlConfiguration.lifetimeInSeconds = 3600;
+      context.samlConfiguration.signResponse = true;
+      context.samlConfiguration.nameIdentifierFormat =
+        "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress";
+      context.samlConfiguration.nameIdentifierProbes = [
+        "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress",
+      ];
+      context.samlConfiguration.authnContextClassRef =
+        "urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport";
+      context.samlConfiguration.signingCert = `-----BEGIN CERTIFICATE-----FAKE-----END CERTIFICATE-----`;
+      context.samlConfiguration.includeAttributeNameFormat = true;
+
       // Multifactor
       context.multifactor = {
         provider: "any",
@@ -57,6 +84,7 @@ describe("example rule", function () {
     await converter.convert(event, rule);
 
     // Assert
+    // ID and Access token claims
     chai
       .expect(api.idToken.setCustomClaim)
       .to.have.been.called.with(
@@ -70,6 +98,14 @@ describe("example rule", function () {
         event.secrets.TEST_SECRET,
       );
     chai
+      .expect(api.accessToken.setCustomClaim)
+      .to.have.been.called.with(
+        "https://example.com/testAccessToken",
+        "testAccessTokenValue",
+      );
+
+    // SAML
+    chai
       .expect(api.samlResponse.setAttribute)
       .to.have.been.called.with(
         "https://example.com/SAML/Attributes/Role",
@@ -82,11 +118,78 @@ describe("example rule", function () {
         "session",
       );
     chai
+      .expect(api.samlResponse.setAudience)
+      .to.have.been.called.with("https://example.com/SAML/Attributes/Audience");
+    chai
+      .expect(api.samlResponse.setIssuer)
+      .to.have.been.called.with("https://example.com/SAML/Attributes/Issuer");
+    chai
+      .expect(api.samlResponse.setEncryptionPublicKey)
+      .to.have.been.called.with(
+        "-----BEGIN PUBLIC KEY-----FAKE-----END PUBLIC KEY-----",
+      );
+    chai
+      .expect(api.samlResponse.setRecipient)
+      .to.have.been.called.with(
+        "https://example.com/SAML/Attributes/Recipient",
+      );
+    chai
+      .expect(api.samlResponse.setCreateUpnClaim)
+      .to.have.been.called.with(true);
+    chai
+      .expect(api.samlResponse.setPassthroughClaimsWithNoMapping)
+      .to.have.been.called.with(true);
+    chai
+      .expect(api.samlResponse.setMapUnknownClaimsAsIs)
+      .to.have.been.called.with(true);
+    chai
+      .expect(api.samlResponse.setMapIdentities)
+      .to.have.been.called.with(true);
+    chai
+      .expect(api.samlResponse.setDestination)
+      .to.have.been.called.with(
+        "https://example.com/SAML/Attributes/Destination",
+      );
+    chai
+      .expect(api.samlResponse.setRelayState)
+      .to.have.been.called.with(
+        "https://example.com/SAML/Attributes/relayState",
+      );
+    chai
       .expect(api.samlResponse.setLifetimeInSeconds)
       .to.have.been.called.with(3600);
     chai
+      .expect(api.samlResponse.setSignResponse)
+      .to.have.been.called.with(true);
+    chai
+      .expect(api.samlResponse.setNameIdentifierFormat)
+      .to.have.been.called.with(
+        "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress",
+      );
+    chai
+      .expect(api.samlResponse.setNameIdentifierProbes)
+      .to.have.been.called.with([
+        "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress",
+      ]);
+    chai
+      .expect(api.samlResponse.setAuthnContextClassRef)
+      .to.have.been.called.with(
+        "urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport",
+      );
+    chai
+      .expect(api.samlResponse.setSigningCert)
+      .to.have.been.called.with(
+        "-----BEGIN CERTIFICATE-----FAKE-----END CERTIFICATE-----",
+      );
+    chai
+      .expect(api.samlResponse.setIncludeAttributeNameFormat)
+      .to.have.been.called.with(true);
+
+    // Multifactor
+    chai
       .expect(api.multifactor.enable)
       .to.have.been.called.with("any", { allowRememberBrowser: false });
+
     // Secrets
     chai.expect(configuration.TEST_SECRET).to.equal(event.secrets.TEST_SECRET);
   });
